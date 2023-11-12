cabc099b9bd2e9dda949be528b8e11668ef1b171
swal0070 <swal0070@student.monash.edu>
2023-11-12 18:17:42 +1100

    streamlit app first push

---
+++ main.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import linregress
+import seaborn as sns  # For a nice palette of colors
+from plots import plot_dissolved_oxygen_trends
+
+st.set_page_config(page_title="Gibbsland Lakes Analysis", page_icon="🧮", layout="wide")
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            <h2 style='text-align: center;'Gibbsland Lakes Analysis</h2>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+st.title('Gibbsland Lakes Analysis')
+
+st.write('Percentage of the Salinity in Gippsland Lakes Over the last 30 years')
+col1, col2, col3, col4, col5 = st.columns(5)
+col1.metric("Lake King North", "26.76", "11.27%")
+col2.metric("Lake King North", "27.63", "4.23%")
+col3.metric("Lake Victoria", "20.72", "16.20%")
+col4.metric("Lake Wellington", "7.56", "21.95%")
+col5.metric("Shaving Point", "27.87", "5.02%")
+
+
+st.write('Percentage of Dissolved Oxygen in Gippsland Lakes Over the last 30 years')
+col1, col2, col3, col4, col5 = st.columns(5)
+col1.metric("Lake King North", "9.64", "27.24%")
+col2.metric("Lake King North", "9.07", "4.01%")
+col3.metric("Lake Victoria", "11.38", "-1.78%")
+col4.metric("Lake Wellington", "7.56", "-5.93%")
+col5.metric("Shaving Point", "8.99", "14.14%")
+
+
+# Load the dataset
+file_path = '/Users/sam/PycharmProjects/gibbslands_web_app/1990_01-2023_06_Gippsland_Lakes_Water_quality_data.xlsx'
+data = pd.read_excel(file_path)
+
+# Calculate the percentage of missing values for each column
+missing_data_percentage = data.isnull().mean() * 100
+
+# Set a threshold for missing data to decide whether to drop the column
+threshold = 50  # percentage
+columns_to_drop = missing_data_percentage[missing_data_percentage > threshold].index.tolist()
+
+# Drop columns with missing data above the threshold
+data_cleaned = data.drop(columns=columns_to_drop)
+
+# Remove any duplicate rows in the dataset
+data_cleaned = data_cleaned.drop_duplicates()
+
+# Rename columns for clarity and consistency
+renamed_data = data_cleaned.rename(columns=lambda x: x.strip().replace(" ", "_").lower())
+
+# Isolate the columns for salinity and dissolved oxygen along with 'date' and 'site_name_short'
+water_quality_data = renamed_data[['sal', 'do_mg', 'date', 'site_name_short']]
+
+# Drop rows where 'sal' or 'do_mg' are missing
+water_quality_comparison_data = water_quality_data.dropna(subset=['sal', 'do_mg'])
+
+water_quality_comparison_data1 = water_quality_comparison_data[water_quality_comparison_data['site_name_short'] != 'Lake_Reeve_East']
+# drop Lake_Reeve_East as its only has data from the last few years
+
+water_quality_trends = water_quality_comparison_data1.groupby(['site_name_short', 'date']).mean().reset_index()
+
+
+
+# Streamlit app
+def main():
+    pass
+    # Load or prepare your data here
+    # water_quality_trends = ...
+
+    # Plotting
+    # fig = plot_dissolved_oxygen_trends(water_quality_trends)
+    # st.pyplot(fig)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ plots.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import linregress
+import seaborn as sns  # For a nice palette of colors
+
+def plot_dissolved_oxygen_trends(water_quality_trends):
+    fig, ax = plt.subplots(figsize=(15, 7))
+
+    # Define a color palette
+    palette = sns.color_palette('husl', n_colors=water_quality_trends['site_name_short'].nunique())
+
+    # Plot the regression line for dissolved oxygen data for each lake
+    for idx, (lake, color) in enumerate(zip(water_quality_trends['site_name_short'].unique(), palette)):
+        # Select data for the current lake
+        lake_data = water_quality_trends[water_quality_trends['site_name_short'] == lake].copy()
+
+        # Convert dates to ordinal numbers for regression
+        lake_data['date_ordinal'] = pd.to_datetime(lake_data['date']).apply(lambda x: x.toordinal())
+
+        # Calculate the linear regression
+        slope, intercept, r_value, p_value, std_err = linregress(lake_data['date_ordinal'], lake_data['do_mg'])
+
+        # Create the regression line
+        reg_line = slope * lake_data['date_ordinal'] + intercept
+
+        # Plot the regression line with a unique color and label
+        ax.plot(lake_data['date'], reg_line, label=lake, color=color, linewidth=2)
+
+    # Title and labels for dissolved oxygen plot
+    ax.set_title('Linear Regression of Dissolved Oxygen Trends in Gippsland Lakes Over Time', fontsize=16)
+    ax.set_xlabel('Year', fontsize=14)
+    ax.set_ylabel('Dissolved Oxygen (mg/L)', fontsize=14)
+
+    # Legend
+    ax.legend(title='Lakes', loc='upper left', bbox_to_anchor=(1, 1))
+
+    # Formatting the date ticks
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+
+    return fig
+
+
